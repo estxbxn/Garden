4105868f3ec64ba2e582ebc7191d5dd89550884e
Estéban Piecourt <esteban.piecourt@gmail.com>
2024-11-15 15:27:30 +0100

    ✨ feat: Add Location class for managing Vegetables

---
+++ src/Location.java
@@ -0,0 +1,23 @@
+import flore.Vegetable;
+
+public class Location {
+
+    private Vegetable vegetable;
+
+    public Location(Vegetable vegetable) {
+        this.vegetable = vegetable;
+    }
+
+    public Vegetable getVegetable() {
+        return vegetable;
+    }
+
+    public void setVegetable(Vegetable vegetable) {
+        this.vegetable = vegetable;
+    }
+
+    @Override
+    public String toString() {
+        return vegetable.getDraw()[vegetable.getState().ordinal()];
+    }
+}
